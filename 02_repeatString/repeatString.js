0bda0093b21c0705a633732aab766cbb0f77aa71
kirillgorokhov <kirillgorokhov@gmail.com>
2022-08-04 21:38:22 +0300

    Complete first 7 tasks in Foundations chapter of TOP

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let bigString = '';
+    if (num <0) {
+        return 'ERROR';
+    }
+    else {
+    for (i=0; i < num; i++) {
+        bigString = bigString + string;
+    }
+    return bigString;
+}
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+let sum = 0;
+if (firstNum < 0 || secondNum < 0 || !Number.isFinite(firstNum) || !Number.isFinite(secondNum)) {
+    return 'ERROR';
+}
+else if (firstNum <= secondNum) {
+for (i = firstNum; i <= secondNum; i++) {
+    sum += i; 
+}
+}
+else {
+    for (i = secondNum; i <= firstNum; i++) {
+        sum += i;   
+} 
+}
+return sum;
 };
 
 // Do not edit below this line
